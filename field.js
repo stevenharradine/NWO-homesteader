db0ec727cd1adde11c5e6468cf4944dc86c6522c
Steven Harradine <stevenharradine@gmail.com>
2017-09-08 19:15:51 -0400

    allows clicking the map, and selecting a tile

--- field.js
+++ field.js
@@ -1,5 +1,9 @@
 document.getElementById ("beginJourney").onclick = function () {
-	drawMap();
+	drawMap()
+	drawMapDetail()
+	drawActions()
+
+	resetMapEventListener ()
 }
 
 var tileTypes = [{
@@ -25,6 +29,28 @@ function getNewSquare () {
 	return probabilityMatrix[Math.floor(Math.random() * probabilityMatrix.length) + 0]
 }
 
+function drawMapDetail () {
+	var buffer = ""
+
+	buffer = "<div class='legend'>"
+	buffer += "<h3>Terrain details:</h3>"
+	buffer += "<ul id='mapDetail'>"
+	buffer += "<li>Terrain: </li>"
+	buffer += "</ul></div>"
+
+	document.getElementById("container").innerHTML += buffer
+}
+
+function drawActions () {
+	var buffer = ""
+
+	buffer = "<div id='actions'>"
+	buffer += "<h3>Actions:</h3>"
+	buffer += "</div>"
+
+	document.getElementById("container").innerHTML += buffer
+}
+
 function drawMap () {
 	var table = document.createElement("table")
 	table.id = "map"
@@ -46,10 +72,6 @@ function drawMap () {
 			tr.appendChild (td)
 		}
 		table.appendChild(tr)
-	}
-
-	table.onclick = function (e) {
-		showTileData (e.target)
 	}
 
 	// clear old screen
@@ -84,21 +106,44 @@ function showTileData (tileData) {
 		if (tileClasses.indexOf("worker") >= 0) {
 			worker = "worker"
 		}
-		var buffer = "<div id='popup'>"
-		buffer += "<div id='tileData'><p>Tile Data</p>"
-		buffer += "<ul>"
+		var buffer = ""
 		buffer += "<li>Type: " + tileType + "</li>"
 		if (building !== "") buffer += "<li>Building: " + building + "</li>"
-		if (worker !== "") buffer += "<li>Worker: " + worker + "</li>"
-		buffer += "</ul>"
-		buffer += "<button onclick='document.getElementById(\"popup\").parentNode.removeChild(document.getElementById(\"popup\"))'>close</button>"
-		buffer += "</div>"
-		buffer += "</div>"
+		if (worker !== "") buffer += "<li>Unit: " + worker + "</li>"
 
-		document.getElementById("container").innerHTML += buffer
+		document.getElementById("mapDetail").innerHTML = buffer
 
-		document.getElementById("map").onclick = function (e) {
-			showTileData (e.target)
+		resetMapEventListener ();
+	}
+}
+
+function deselectAll () {
+	var tds = document.getElementsByTagName("td")
+
+	for (var i = 0; i < tds.length; i++) {
+		if (tds[i].className.indexOf("selected"))
+			tds[i].className = tds[i].className.replace ("selected", "")
+	}
+}
+
+function resetMapEventListener () {
+	document.getElementById("map").onmouseover = function (e) {
+		showTileData (e.target)
+	}
+	document.getElementById("map").onclick = function (e) {
+		deselectAll()
+		e.target.className += " selected"
+		var unitType = ""
+
+		if (e.target.className.indexOf ("worker") >= 0) {
+			unitType = "Worker"
 		}
+		var buffer = "<h3>Actions: " + unitType + "</h3>"
+
+		if (unitType === "Worker") {
+			buffer += "Goto"
+		}
+
+		document.getElementById ("actions").innerHTML = buffer
 	}
 }
